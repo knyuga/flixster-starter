95b0cbfda71e4573df5381c46749fdb8b184e413
Kenna Nyuga-Galega <knyugagalega@knyugag-ltmuj44.internal.salesforce.com>
2025-06-13 15:42:46 -0700

    added basic CSS

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import MovieList from "./Components/MovieList/MovieList";
 const App = () => {
   return (
     <>
+    <header id="App-header">FLIXSTER</header>
     <MovieList />
     </>
   );
--- src/Components/MovieList/MovieList.jsx
+++ src/Components/MovieList/MovieList.jsx
@@ -1,6 +1,5 @@
 import {useState, useEffect} from "react";
 import axios from "axios";
-// import MovieCard from "../Components/MovieCard/MovieCard";
 import MovieCard from "../MovieCard/MovieCard";
 import "./MovieList.css";
 
